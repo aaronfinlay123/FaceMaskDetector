353d0262af3a63558614c1f932659d447b1292e2
Kyle Hennessy <kyle-hennessy@hotmail.com>
2021-02-06 17:11:28 +0000

    Added comments to implementation.py which give an explantation of what the code is doing

--- src/implementation.py
+++ src/implementation.py
@@ -2,42 +2,54 @@ from keras.models import load_model
 import cv2
 import numpy as np
 
-model = load_model('model-020.model')
+#Loading the model that has the highest accuracy and lowest loss
+trainedmodel = load_model('model-050.model')
 
-face_clsfr=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#The haar cascade classifier algorithm is being used to locate the face
+faceclassifier=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-source=cv2.VideoCapture(0)
+#Opens the default web camera for the device
+webcamera=cv2.VideoCapture(0)
 
-labels_dict={0:'NO MASK',1:'MASK'}
-color_dict={0:(0,0,255),1:(0,255,0)}
+#Dictionaries that contain the labels and the colours for the bounding boxes
+labels={0:'NO MASK',1:'MASK'}
+colours={0:(0,0,255),1:(0,255,0)}
 
-
+#keeps looping as long as the web camera is open
 while(True):
+    #reads in the frame from the webcam
+    ret,frame=webcamera.read()
+    #converts the image to gray scale to normalise the data
+    grey=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces=faceclassifier.detectMultiScale(grey,1.3,5)
 
-    ret,img=source.read()
-    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    faces=face_clsfr.detectMultiScale(gray,1.3,5)  
-
+    #for each frame in faces do this:
     for (x,y,w,h) in faces:
-    
-        face_img=gray[y:y+w,x:x+w]
-        resized=cv2.resize(face_img,(100,100))
-        normalized=resized/255.0
-        reshaped=np.reshape(normalized,(1,100,100,1))
-        result=model.predict(reshaped)
+        #normalise and reshape the face
+        face=grey[y:y+w,x:x+w]
+        resized=cv2.resize(face,(100,100))
+        normalised=resized/255.0
+        reshaped=np.reshape(normalised,(1,100,100,1))
+        #predict if the face is wearing a mask or not
+        result=trainedmodel.predict(reshaped)
 
+        #save the labels that were found in prediction
         label=np.argmax(result,axis=1)[0]
-      
-        cv2.rectangle(img,(x,y),(x+w,y+h),color_dict[label],2)
-        cv2.rectangle(img,(x,y-40),(x+w,y),color_dict[label],-1)
-        cv2.putText(img, labels_dict[label], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+
+        #Draw the bounding box around the face and the result i.e 'MASK' and green box or 'NO MASK' and red box
+        cv2.rectangle(frame,(x,y),(x+w,y+h),colours[label],2)
+        cv2.rectangle(frame,(x,y-40),(x+w,y),colours[label],-1)
+        cv2.putText(frame, labels[label], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
         
-        
-    cv2.imshow('LIVE',img)
+    #display window containing the image
+    cv2.imshow('Face mask detector by Kyle Hennessy, Jordan Marah, Aaron Finlay',frame)
+    #wait for a key press
     key=cv2.waitKey(1)
     
+    #if the key press is "escape" then we break out of the loop
     if(key==27):
         break
-        
+
+#Destroy the window after the loop has finished and release the webcam     
 cv2.destroyAllWindows()
-source.release()
+webcamera.release()
